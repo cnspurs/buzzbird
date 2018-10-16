708411513e9ff612e1b4b161452b7cf16a862718
Aris Chow <arischow@gmail.com>
2018-10-16 10:20:18 +0800

    DEBUG=False in production

--- buzzbird/settings.py
+++ buzzbird/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'voc2qq6&#)(!+b%d+4*5hgvjsyj!)xfs#c%*mj90pvrf2kcre1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['buzzbird.cnspurs.com']
 
