642b78253a202c46323b53200fb9e1e560f5f039
Aris Chow <arischow@gmail.com>
2018-10-16 10:38:38 +0800

    init_admin command for creating a superuser in Docker container

---
+++ core/management/commands/init_admin.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        admin = User.objects.filter(username='arischow').first()
+
+        if admin:
+            self.stdout.write('Overlord arischow existed.')
+        else:
+            User.objects.create_superuser(username='arischow',
+                                          email='arischow@gmail.com',
+                                          password='cnspurs8633')
+            self.stdout.write('Admin created.')
