1d5261e84b0ca9a065b1076b78b8eb97898e2877
Aris Chow <arischow@gmail.com>
2018-10-16 14:08:43 +0800

    admin urlconf

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.contrib import admin
 from core import views
 
 urlpatterns = (
     path('', views.index),
     path('oauth', views.oauth),
     path('logout', views.logout_view, name='logout'),
+    path('whosyourdaddy', admin.site.urls)
 )
