752eed04fa0b65188577f727e2380bb05ebd1064
Aris Chow <arischow@gmail.com>
2018-10-16 21:44:19 +0800

    static folder & related settings

--- buzzbird/settings.py
+++ buzzbird/settings.py
@@ -120,9 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 weibo_app_id = os.getenv('WEIBO_APP_ID')
 weibo_app_secret = os.getenv('WEIBO_APP_SECRET')
