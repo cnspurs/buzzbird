3837df10025052ab5665d9ebdd84ff2db7b13158
Aris Chow <arischow@gmail.com>
2018-10-22 14:04:26 +0800

    SQLite -> PostgreSQL

---
+++ README.md
@@ -0,0 +1,11 @@
+# Environment variables
+## Database
+`DATABASE_HOSTNAME`
+
+`DATABSE_PORT`
+
+`DATABASE_USERNAME`
+
+`DATABASE_PASSWORD`
+
+`DATABASE_DBNAME`
--- buzzbird/settings.py
+++ buzzbird/settings.py
@@ -75,10 +75,21 @@ WSGI_APPLICATION = 'buzzbird.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DATABASE_DBNAME'),
+        'USER': os.getenv('DATABASE_USERNAME'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'HOST': os.getenv('DATABASE_HOSTNAME'),
+        'PORT': os.getenv('DATABASE_PORT'),
     }
 }
 
