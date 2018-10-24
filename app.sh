07bb41dd74d833dfc088b9d38474845422043c5d
Aris Chow <arischow@gmail.com>
2018-10-24 21:42:12 +0800

    wait for the launch of PgSQL

--- app.sh
+++ app.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+echo "Waiting for postgres..."
+
+while ! nc -z db 5432; do
+  sleep 0.1
+done
+
+echo "PostgreSQL started"
+
 python3 manage.py migrate --noinput
 python3 manage.py init_admin
 python3 manage.py init_twittermember
