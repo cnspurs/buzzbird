9b0934cf77b06c056c904a84b47a3dccff3f7eb0
Aris Chow <arischow@gmail.com>
2019-03-19 23:36:51 +0800

    dotenv removed

--- buzzbird/wsgi.py
+++ buzzbird/wsgi.py
@@ -10,8 +10,6 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from dotenv import load_dotenv
-load_dotenv()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'buzzbird.settings')
 
