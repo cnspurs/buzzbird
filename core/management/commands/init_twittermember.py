3202f0eb0a81ec61d7c81dd7bcbca99479b01588
Aris Chow <arischow@gmail.com>
2019-07-02 21:32:10 +0800

    fix init command

--- core/management/commands/init_twittermember.py
+++ core/management/commands/init_twittermember.py
@@ -1,20 +1,21 @@
 from django.core.management.base import BaseCommand
-from core.models import TwitterMember
+from core.models import Member
 from core.utils import twitter
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        if TwitterMember.objects.count() != 0:
+        if Member.objects.twitter().count() != 0:
             return
 
         members = twitter.get_list_members()
 
         count = 0
         for user in members:
-            TwitterMember.objects.create(twitter_id=user.id_str,
-                                         english_name=user.name)
+            Member.objects.create(twitter_id=user.id_str,
+                                  english_name=user.name,
+                                  type='twitter')
 
             count += 1
         self.stdout.write(f'Initialized. {count} members added.')
