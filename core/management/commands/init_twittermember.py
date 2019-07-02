9b55cc1919146f46d9cfe880f434298feca110f5
Aris Chow <arischow@gmail.com>
2019-07-02 23:00:15 +0800

    improve init command

--- core/management/commands/init_twittermember.py
+++ core/management/commands/init_twittermember.py
@@ -6,17 +6,15 @@ from core.utils import twitter
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        if Member.objects.twitter().count() != 0:
-            return
-
         members = twitter.get_list_members()
 
         count = 0
         for user in members:
-            Member.objects.create(twitter_id=user.id_str,
-                                  english_name=user.name,
-                                  type='twitter')
+            if Member.objects.filter(twitter_id=user.id_str).first() is not None:
+                Member.objects.create(twitter_id=user.id_str,
+                                      english_name=user.name,
+                                      type='twitter')
 
-            count += 1
+                count += 1
         self.stdout.write(f'Initialized. {count} members added.')
 
