d5d4a93f6940b298f0191a80f187252ea9936c02
Aris Chow <arischow@gmail.com>
2019-07-02 20:39:22 +0800

    Refactor model

--- core/admin.py
+++ core/admin.py
@@ -7,13 +7,7 @@ class ProfileAdmin(admin.ModelAdmin):
     list_display = ('id', 'user', 'access_token', 'access_token_expired_at')
 
 
-@admin.register(TwitterMember)
-class TwitterMemberAdmin(admin.ModelAdmin):
-    list_display = ('twitter_id', 'english_name', 'chinese_name')
-    readonly_fields = ('twitter_id',)
-
-
-@admin.register(InstagramMember)
+@admin.register(Member)
 class InstagramMemberAdmin(admin.ModelAdmin):
     pass
 
--- core/models.py
+++ core/models.py
@@ -18,13 +18,7 @@ class Profile(models.Model):
         return True
 
 
-class TwitterMember(models.Model):
-    twitter_id = models.CharField('用户推特 ID', max_length=128, unique=True)
-    english_name = models.CharField('英文名', max_length=64)
-    chinese_name = models.CharField('中文译名', max_length=16, null=True)
-
-
-class InstagramMember(models.Model):
+class Member(models.Model):
     english_name = models.CharField(max_length=64, primary_key=True)
     chinese_name = models.CharField(max_length=16)
 
@@ -48,23 +42,12 @@ class Instagram(models.Model):
     link = models.URLField(db_index=True)
     created_at = models.DateTimeField()
     title = models.CharField(blank=True, max_length=1024)
-    user = models.ForeignKey('core.InstagramMember', related_name='posts', null=True, on_delete=models.SET_NULL)
+    user = models.ForeignKey('core.Member', related_name='posts', null=True, on_delete=models.SET_NULL)
+    metadata = JSONField()
 
     def __str__(self):
         return f'<{self.__class__.__name__}: {self.id}, {self.user.english_name}:{self.title}, {self.created_at},' \
-               f'is_buzzbird: {self.is_buzzbird}, is_discourse: {self.is_discourse}>'
-
-
-class Twitter(models.Model):
-    collected_at = models.DateTimeField(auto_now_add=True)
-    is_buzzbird = models.BooleanField('Published to buzzbird Weibo?', default=False)
-    is_discourse = models.BooleanField('Published to discourse?', default=False)
-    media_url = models.URLField(max_length=1024)
-    link = models.URLField(max_length=1024)
-    created_at = models.DateTimeField('Published on Twitter at')
-    title = models.CharField(blank=True, max_length=1024)
-    user = models.ForeignKey('core.TwitterMember', related_name='posts', null=True, on_delete=models.SET_NULL)
-    metadata = JSONField()
+            f'is_buzzbird: {self.is_buzzbird}, is_discourse: {self.is_discourse}>'
 
 
 @receiver(post_save, sender=User)
