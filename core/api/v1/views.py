b7b3dfc5c18f26ed67bd8ab28a7628c5f3462282
Aris Chow <arischow@gmail.com>
2019-07-03 15:21:18 +0800

    Ping API

---
+++ core/api/v1/views.py
@@ -0,0 +1,10 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view()
+def ping(request):
+    print('hello')
+    return Response({
+        'message': 'pong!'
+    })
