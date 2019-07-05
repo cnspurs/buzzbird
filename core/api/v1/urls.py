f97bae89d6ff5543d84ae58459e1918cbf154055
Aris Chow <arischow@gmail.com>
2019-07-05 18:04:45 +0800

    FeedDetail API

--- core/api/v1/urls.py
+++ core/api/v1/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('ping', views.ping),
     path('feeds', views.FeedList.as_view()),
+    path('feeds/<int:pk>', views.FeedDetail.as_view()),
 ]
--- core/api/v1/views.py
+++ core/api/v1/views.py
@@ -30,11 +30,34 @@ class BBListModelMixin:
         })
 
 
+class BBDetailModelMixin(object):
+    """
+    Retrieve a model instance.
+    """
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response({
+            'data': serializer.data,
+        })
+
+
 class BBListAPIView(BBListModelMixin, generics.GenericAPIView):
     def get(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
 
 
+class BBDetailAPIView(BBDetailModelMixin, generics.GenericAPIView):
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+
 class FeedList(BBListAPIView):
     queryset = Feed.objects.all().order_by('-created_at')[:100]
+    serializer_class = FeedSerializer
+
+
+class FeedDetail(BBDetailAPIView):
+    queryset = Feed.objects.all()
     serializer_class = FeedSerializer
