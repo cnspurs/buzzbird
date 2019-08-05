4ba0c110738df3be5a8f8dd1ff067f3e482e5580
Haowen Xu <rommy.davis@gmail.com>
2019-08-05 21:08:30 +0800

    Added get_user_info using weibo.cn API

---
+++ core/weibo_v2.py
@@ -0,0 +1,28 @@
+import sys
+import requests
+
+url = 'https://m.weibo.cn/api/container/getIndex'
+
+
+def standardize_info(info_dict):
+    for k, v in info_dict.items():
+        if 'int' not in str(type(v)) and 'long' not in str(type(v)) and 'bool' not in str(type(v)):
+            info_dict[k] = v.replace(u"\u200b", "").encode(sys.stdout.encoding, "ignore").decode(sys.stdout.encoding)
+    return info_dict
+
+
+def get_user_info(user_id):
+    data = {
+        'containerid': '100505' + user_id
+    }
+    r = requests.get(url, data)
+    json = r.json()
+
+    if not json['ok']:
+        return None
+
+    info = json['data']['userInfo']
+    if info.get('toolbar_menus'):
+        del info['toolbar_menus']
+    user_info = standardize_info(info)
+    return user_info
