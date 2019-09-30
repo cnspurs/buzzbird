a0dbf0817218c9154616ebb1957a7476c84b693e
Aris Chow <arischow@gmail.com>
2019-09-30 16:22:20 +0800

    fix script

--- core/management/commands/ensure_media_objects.py
+++ core/management/commands/ensure_media_objects.py
@@ -10,17 +10,14 @@ class Command(BaseCommand):
     """
 
     def handle(self, *args, **options):
-        feeds = Feed.objects.all().exclude(type='twitter', media_url=None).prefetch_related('media')
+        feeds = (
+            Feed.objects.all()
+            .exclude(type='twitter', media_url=None)
+            .prefetch_related('media')
+        )
         for feed in feeds:
             media = Media.objects.filter(feed=feed)
             if media:
                 for m in media:
                     if m.downloaded is False:
                         async_task(m.download_to_local)
-
-            if not media and feed.media_url:
-                media = Media.objects.create(
-                    feed=feed,
-                    original_url=feed.media_url,
-                )
-                async_task(media.download_to_local)
