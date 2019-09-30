0b3eff15dc8bd4e4f6ccdcf892d8d133e3900bba
Aris Chow <arischow@gmail.com>
2019-09-30 17:17:55 +0800

    update_member

--- core/management/commands/merge_member.py
+++ core/management/commands/merge_member.py
@@ -1,5 +1,4 @@
 from django.core.management.base import BaseCommand
-from django_q.tasks import async_task
 
 from core.models import *
 
---
+++ core/management/commands/update_member.py
@@ -0,0 +1,28 @@
+from django.core.management.base import BaseCommand
+
+from core.models import *
+
+
+class Command(BaseCommand):
+    """
+    merge member
+    """
+
+    def add_arguments(self, parser):
+        parser.add_argument('id', type=int, help='Member ID')
+        parser.add_argument('--instagram', type=str)
+        parser.add_argument('--twitter', type=str)
+        parser.add_argument('--weibo', type=str)
+
+    def handle(self, *args, **kwargs):
+        member_id = kwargs.pop('id')
+
+        member = Member.objects.filter(id=member_id).first()
+
+        if member:
+            for k, v in kwargs:
+                setattr(member, f'{k}_id', v)
+            member.save()
+
+        else:
+            self.stdout.write(self.style.ERROR('Member does not exist!'))
