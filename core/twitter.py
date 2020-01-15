44ccda82e9214e67992f8b8429460824a65b23f1
Aris Chow <arischow@gmail.com>
2020-01-15 17:51:48 +0800

    improvements on displaying text on buzzbird

--- core/twitter.py
+++ core/twitter.py
@@ -13,13 +13,17 @@ from core.schema import Weibo
 from core.utils import Status
 from core.utils import twitter as t
 
-logger = logging.getLogger('core.twitter')
+logger = logging.getLogger("core.twitter")
 
 
 def create_user(twitter: Status):
-    tm = Member.objects.filter(Q(twitter_id=twitter.twitter_user_id) | Q(english_name=twitter.screen_name)).first()
+    tm = Member.objects.filter(
+        Q(twitter_id=twitter.twitter_user_id) | Q(english_name=twitter.screen_name)
+    ).first()
     if not tm:
-        tm = Member.objects.create(twitter_id=twitter.twitter_user_id, english_name=twitter.screen_name)
+        tm = Member.objects.create(
+            twitter_id=twitter.twitter_user_id, english_name=twitter.screen_name
+        )
     tm.twitter_id = twitter.twitter_user_id
     tm.save()
     return tm
@@ -28,19 +32,33 @@ def create_user(twitter: Status):
 def save_content(user, item: Status):
     # if saved, then don't duplicate
     link = item.link
-    twitter = Feed.objects.twitter().filter(Q(status_id=item.tweet_id) | Q(metadata__id_str=item.raw_json['id_str'])).first()
+    twitter = (
+        Feed.objects.twitter()
+        .filter(
+            Q(status_id=item.tweet_id) | Q(metadata__id_str=item.raw_json["id_str"])
+        )
+        .first()
+    )
     if twitter:
         return twitter
 
     created_at = parse(item.created_at)
-    twitter = Feed.objects.create(author=item.author, link=link, created_at=created_at, title=item.text,
-                                  user=user, type='twitter', metadata=item.raw_json, status_id=item.tweet_id)
+    twitter = Feed.objects.create(
+        author=item.author,
+        link=link,
+        created_at=created_at,
+        title=item.text,
+        user=user,
+        type="twitter",
+        metadata=item.raw_json,
+        status_id=item.tweet_id,
+    )
 
     for url in item.images:
         m = Media.objects.create(feed=twitter, original_url=url)
         async_task(m.download_to_local)
 
-    logger.info(f'Twitter: {twitter} saved')
+    logger.info(f"Twitter: {twitter} saved")
     return twitter
 
 
@@ -58,13 +76,13 @@ def get_image(url):
 
 
 def twitter_to_weibo(twitter: Feed):
-    text = f'【{twitter.user.name} 推特】{twitter.title[:110]}... https://spursnews.net/weibo/instagrams/{twitter.id}'
+    text = f"【{twitter.user.name} 推特】{twitter.title[:120]} ... https://spursnews.net/weibo/instagrams/{twitter.id}"
     media = None
     if twitter.media.count() > 0:
         media = twitter.media.all()[0]
     data = {
-        'text': text,
-        'pic': func.get_local_image(media.local_path) if media is not None else None,
-        'tweet_id': twitter.id,
+        "text": text,
+        "pic": func.get_local_image(media.local_path) if media is not None else None,
+        "tweet_id": twitter.id,
     }
     return Weibo(**data)
