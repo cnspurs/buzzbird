e367c9f24b1054f7fa15695a7c4eb0784df261a0
Aris Chow <arischow@gmail.com>
2020-01-15 15:41:56 +0800

    remove useless code

--- core/cron.py
+++ core/cron.py
@@ -12,34 +12,6 @@ logger = logging.getLogger('core.cron')
 oauth_weibo = settings.WEIBO
 
 
-def sync():
-    profile = Profile.objects.filter(user__username='5833511420').first()
-
-    if profile is None:
-        return
-
-    last_tweet_id = Settings.last_tweet_id()
-
-    timeline = t.get_timeline(since_id=last_tweet_id.value)
-    timeline.reverse()
-
-    count = 0
-    for tweet in timeline:
-        weibo = tweet.to_weibo()
-        if weibo is not None:
-            result = oauth_weibo.post(profile, weibo)
-
-            if result:
-                last_tweet_id.value = weibo.tweet_id
-                last_tweet_id.save()
-                count += 1
-            seconds = random.randint(15, 45)
-            time.sleep(seconds)
-            logger.info(f'Tweets: Published. Sleep {seconds} seconds')
-
-    logger.info(f'Cron job finished. Length: {len(timeline)}, sent: {count}')
-
-
 def sync_instagram():
     sync_instagram_to_weibo()
     sync_instagram_to_discourse()
