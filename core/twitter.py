4858e160c7ad70acda1f6caeb9bd5a69001c8326
Aris Chow <arischow@gmail.com>
2022-05-28 08:58:25 +0800

    Won't stop retrieving if exceptions raise

--- core/twitter.py
+++ core/twitter.py
@@ -7,6 +7,8 @@ from dateutil.parser import parse
 from django.db.models import Q
 from django_q.tasks import async_task
 
+from twitter.error import TwitterError
+
 from core.models import Feed, Member, Media
 from core.utils import Status
 from core.utils import twitter as t
@@ -69,17 +71,20 @@ def save_contents(full_sync=False):
         last_feed: Feed = Feed.objects.twitter().filter(user_id=m.id).order_by('-status_id').first()
         if last_feed:
             kwargs['since_id'] = int(last_feed.status_id)
+        logger.debug("Twitter: Ready to fetch tweets for {}".format(m.english_name))
+        try:
+            tl = t.get_user_timelime(m, **kwargs)
 
-        tl = t.get_user_timelime(m, **kwargs)
+            if not last_feed and full_sync is False:
+                if m.synced_from is not None:
+                    tl = [status for status in tl if parse(status.created_at) > m.synced_from]
+                else:
+                    logger.error(f'ID {m.id}, {m.english_name} should have a sync_from datetime but it doesn\'t.')
 
-        if not last_feed and full_sync is False:
-            if m.synced_from is not None:
-                tl = [status for status in tl if parse(status.created_at) > m.synced_from]
-            else:
-                logger.error(f'ID {m.id}, {m.english_name} should have a sync_from datetime but it doesn\'t.')
-
-        for twitter in tl:
-            save_content(m, twitter)
+            for twitter in tl:
+                save_content(m, twitter)
+        except TwitterError as e:
+            logger.error(f"Twitter: {e}")
 
 
 def get_image(url):
